3b3c14f7b66c22b3386f5652cc4b39ba361394db
Edeson Junior <edeson.jsbarbosa@hotmail.com>
2024-02-20 11:06:30 -0300

    Iniciando um Novo Projeto: Interação com o Banco de Dados

---
+++ app.js
@@ -0,0 +1,36 @@
+/****************************************************************
+ * 
+ * MVC - (model, view, controller)
+ * 
+ * Model -> Modelagem dos dados (BD)
+ * Controller -> É responsável pela regra de negócio do projeto
+ * View -> É responsável pela interação com o Cliente (usuário)
+ * 
+ ****************************************************************/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
