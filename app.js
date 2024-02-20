c917273cd712b5df8bef35cd84a12f1599535b8c
Edeson Junior <edeson.jsbarbosa@hotmail.com>
2024-02-20 15:19:43 -0300

    Implementação da API Integrada com o Banco de Dados

--- app.js
+++ app.js
@@ -1,12 +1,34 @@
 /****************************************************************
- * 
+ * Objetivo: API para realizar requisição de contatos da UniFECAF
+ * Data: 20/02/2024
+ * Autor: Edeson Barbosa
+ * Version: 1.0
+ *****************************************************************/ 
+ /****************************************************************
  * MVC - (model, view, controller)
  * 
  * Model -> Modelagem dos dados (BD)
  * Controller -> É responsável pela regra de negócio do projeto
  * View -> É responsável pela interação com o Cliente (usuário)
  * 
+ * Dependências para acesso a banco de dados:
+ * - Sequelize
+ * - Prisma - será usado neste projeto.
+ * - Fastfy
+ * 
+ *  Comandos de instalação e configuração do Prisma: 
+ *      * Instalação: npm install prisma --save
+ *      * Inicialização do prisma: npx prisma init
+ *      * Instalação do prisma client: npm install @prisma/client --save
+ * 
+ *  Criando uma tabela teste no mysql através do prisma
+ *      * npx prisma migrate dev
  ****************************************************************/
+
+
+
+
+
 
 
 
